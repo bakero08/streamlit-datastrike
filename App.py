7109db31c209cb6ad1029ad23068d1f76f252e5e
Bakero08 <gaur.saurabh2907@gmail.com>
2022-06-12 05:45:22 +0100

    Update App.py

--- App.py
+++ App.py
@@ -55,8 +55,9 @@ def app():
         teamSelect = st.selectbox(
             "Choose the Team:", Teams)
         
+        crestName = 'FA_WSL\\'+teamSelect+'.png'
         
-        crest = Image.open('FA_WSL\f'{teamSelect}'.png')
+        crest = Image.open(crestName)
         col1.image(crest)
         
                 
